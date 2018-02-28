0c1b286fb7248345c03be188fde49db53f65c50a
hunter36 <673389081@qq.com>
2018-02-28 20:10:06 +0800

    登陆注册添加了正则检测

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,13 +1,27 @@
 import React ,{Component} from 'react';
 import '../../styles/cases/LoginHead.css';
 import '../../styles/cases/LoginAndRegister.css';
-import { Button, WhiteSpace } from 'antd-mobile';
+import { Button, WhiteSpace , Toast} from 'antd-mobile';
 
 class Login extends Component {
 	constructor(props){
 		super(props);
 
 		this.gotoRegister = this.gotoRegister.bind(this);
+		this.checkData = this.checkData.bind(this);
+	}
+	checkData(){
+		const phone = document.getElementById('loginName').value;
+		const pwd = document.getElementById('loginPwd').value;
+		if(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(pwd) === false){
+			document.getElementById('pwdTest').style.display = "block";
+		}
+		if(/^1[3,4,5,7,8]\d{9}$/.test(phone) === false){
+			document.getElementById('phoneTest').style.display = "block";
+		}
+		if(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(pwd) && /^1[3,4,5,7,8]\d{9}$/.test(phone)){
+			Toast.success('登录成功', 1);
+		}
 	}
 	gotoRegister(){
 		this.props.history.push("/register");
@@ -16,21 +30,21 @@ class Login extends Component {
 		return (
 			<div>
 				<div className="login_head">
-					<a href="/" className="head_logo"></a>
+					<a href="/" className="head_logo">&nbsp;</a>
 					<span className="logo_text">登录</span>
 				</div>
 				<div className="form">
 					<div className="input_box">
-						<input type="text" placeholder="你的手机号" id=""/>
+						<input type="text" placeholder="你的手机号" id="loginName"/>
 					</div>
-					<span className="tip">请输入注册时用的邮箱或者手机号呀</span>
+					<span className="tip" id="phoneTest">请输入注册时用的邮箱或者手机号呀</span>
 					<div className="input_box">
-						<input type="text" placeholder="密码" id=""/>
+						<input type="password" placeholder="密码" id="loginPwd"/>
 					</div>
-					<span className="tip">喵，你没输入密码么？</span>
+					<span className="tip" id="pwdTest">喵，你没输入密码么？</span>
 					<p className="forget_pwd">忘记密码?</p>
 					<Button type="primary" style={{background: '#00a1d6',
-    color:'#fff',borderRadius: '32px'}}>立即登录</Button><WhiteSpace />
+    color:'#fff',borderRadius: '32px'}} onClick={this.checkData}>立即登录</Button><WhiteSpace />
 					<Button style={{background: '#fff',
     color:'#00a1d6',borderRadius: '32px'}} onClick={this.gotoRegister}>注册</Button><WhiteSpace />
 				</div>
--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,7 +1,7 @@
 import React ,{Component} from 'react';
 import '../../styles/cases/LoginHead.css';
 import '../../styles/cases/LoginAndRegister.css';
-import { Button, WhiteSpace } from 'antd-mobile';
+import { Button, WhiteSpace,Toast } from 'antd-mobile';
 
 
 class Register extends Component {
@@ -9,33 +9,50 @@ class Register extends Component {
 		super(props);
 
 		this.gotoLogin = this.gotoLogin.bind(this);
+		this.checkText = this.checkText.bind(this);
 	}
 	gotoLogin(){
 		this.props.history.push("/login");
+	}
+	checkText(){
+		const pwd = document.getElementById('upassword').value;
+		const phone = document.getElementById('uphone').value;
+		if(!document.getElementById('uname').value){
+			document.getElementById('nameTip').style.display = "block";
+		}
+		if(/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(pwd) === false){
+			document.getElementById('pwdTip').style.display = "block";
+		}
+		if(/^1[3,4,5,7,8]\d{9}$/.test(phone) === false){
+			document.getElementById('phoneTip').style.display = "block";
+		}
+		if(document.getElementById('uname').value && /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$/.test(pwd) && /^1[3,4,5,7,8]\d{9}$/.test(phone)){
+			Toast.success('注册成功', 1);
+		}
 	}
 	render(){
 		return (
 			<div>
 				<div className="login_head">
-					<a href="/" className="head_logo"></a>
+					<a href="/" className="head_logo">&nbsp;</a>
 					<span className="logo_text">注册</span>
 				</div>
 				<div className="form">
 					<div className="input_box">
-						<input type="text" placeholder="您的昵称, 例：哔哩哔哩" id=""/>
+						<input type="text" placeholder="您的昵称, 例：哔哩哔哩" id="uname"/>
 					</div>
-					<span className="tip">这个昵称已经有人用过了</span>
+					<span className="tip" id="nameTip">这个昵称已经有人用过了</span>
 					<div className="input_box">
-						<input type="text" placeholder="您的密码, 6-16个字符组成，区分大小写" id=""/>
+						<input type="password" placeholder="您的密码, 6-16个字符组成，区分大小写" id="upassword"/>
 					</div>
-					<span className="tip">设个密码守护你吧！</span>
+					<span className="tip" id="pwdTip">设个密码守护你吧！</span>
 					<div className="input_box">
-						<input type="text" placeholder="您的手机" id=""/>
+						<input type="text" placeholder="您的手机" id="uphone"/>
 					</div>
-					<span className="tip">亲，看起来不像手机号呢！</span>
+					<span className="tip" id="phoneTip">亲，看起来不像手机号呢！</span>
 					<p>继续注册，表明您已同意<b>《bilibili网会员注册协议》</b></p>
 					<Button type="primary" style={{background: '#00a1d6',
-    color:'#fff',borderRadius: '32px'}}>同意并注册</Button><WhiteSpace />
+    color:'#fff',borderRadius: '32px'}} onClick={this.checkText}>同意并注册</Button><WhiteSpace />
 					<Button style={{background: '#fff',
     color:'#00a1d6',borderRadius: '32px'}} onClick={this.gotoLogin}>登录</Button><WhiteSpace />
 				</div>
