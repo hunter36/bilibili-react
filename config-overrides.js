b241e1ad5d8f0d9e55a882be52c72cd81165b306
hunter36 <673389081@qq.com>
2018-03-06 11:40:36 +0800

    准备搭载sass

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,8 @@
 const { injectBabelPlugin } = require('react-app-rewired');
+const fs = require('fs');
 module.exports = function override(config, env) {
+	fs.writeFileSync('before.txt',JSON.stringify(config));
 	config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
+	fs.writeFileSync('after.txt',JSON.stringify(config));
 	return config;
 };
